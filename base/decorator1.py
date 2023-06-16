be1aaf19a684b93f396a5886cf7ba75d1466ebcf
Gennadiy <renardes@gmail.com>
2023-06-16 11:15:48 +0500

    основы написания декораторов

---
+++ base/decorator1.py
@@ -0,0 +1,45 @@
+def decorator(function):
+    print('\tначало выполнения функции-декоратора')
+    
+    def wrapper(*args, **kwargs):
+        print('\t\tначало выполнения функции-обёртки')
+        result = function(*args, **kwargs)
+        print('\t\tзавершение выполнения функции-обёртки')
+        return result
+    
+    print('\tзавершение выполнения функции-декоратора')
+    return wrapper
+
+
+def test_func():
+    """демонстратор декорируемой функции"""
+    print('\t\t\tвыполнение декорируемой функции')
+
+
+print(f'{test_func}\n{test_func.__name__}\n{test_func.__doc__}\n')
+print('начало декорирования функции test_func')
+
+test_func = decorator(test_func)
+
+print('завершение декорирования функции test_func')
+print(f'{test_func}\n{test_func.__name__}\n{test_func.__doc__}\n')
+
+test_func()
+
+print()
+
+
+def adder(x, y):
+    return x + y
+
+
+adder = decorator(adder)
+print(adder(3.5, 9), end='\n\n')
+
+
+@decorator
+def multiplier(x, y):
+    return x * y
+
+print(f'{multiplier}\n{multiplier.__name__}\n')
+print(multiplier(1/3, 90))
