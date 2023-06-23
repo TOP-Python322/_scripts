177b9ecc3c2547377f908b8caee5da108af4fece
Gennadiy <renardes@gmail.com>
2023-06-23 14:30:37 +0500

    абсолютные и относительные пути, способы записи путей с помощью строковых литералов

---
+++ base/path1.py
@@ -0,0 +1,15 @@
+abs_path1 = 'C:\Windows\System32\cmd.exe'
+abs_path2 = '/var/bash'
+
+
+abs_path3 = 'C:\\Users\\Ivan\\Documents\\file.txt'
+rel_path1 = r'Documents\file.txt'
+
+
+abs_path4 = r'C:\Users\Ivan'
+rel_path2 = r'..\Tanya\Documents\list.txt'
+
+
+abs_path5 = '/home/ivan/documents/file.txt'
+rel_path3 = 'documents/file.txt'
+
