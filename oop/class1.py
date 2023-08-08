f69d63c58c0ca334b1283457b68bd4d6abcdcedf
Gennadiy <renardes@gmail.com>
2023-08-08 21:31:24 +0500

    объект класса и объект экземпляра, ссылки

--- oop/class1.py
+++ oop/class1.py
@@ -16,3 +16,34 @@ anna.username = 'anutka2002'
 anna.password = 'qwerty'
 
 
+# объект класса
+# >>> User
+# <class '__main__.User'>
+
+# объекты экземпляров
+# >>> ivan
+# <__main__.User object at 0x0000023AF25408D0>
+# >>> anna
+# <__main__.User object at 0x0000023AF2540A50>
+
+# ссылки на объект класса в экземпляре
+# >>> type(ivan)
+# <class '__main__.User'>
+# >>>
+# >>> ivan.__class__
+# <class '__main__.User'>
+# >>>
+# >>> type(ivan) is ivan.__class__ is User
+# True
+
+# базовые типы — это тоже классы
+# >>> text = 'строка'
+# >>>
+# >>> type(text)
+# <class 'str'>
+# >>>
+# >>> text.__class__
+# <class 'str'>
+# >>>
+# >>> type(text) is text.__class__ is str
+# True
