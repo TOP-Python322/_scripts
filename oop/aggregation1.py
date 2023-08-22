ac279ce8bb190a8e26aaf25d6ef7c61b0b4789c2
Gennadiy <renardes@gmail.com>
2023-08-22 21:41:03 +0500

    пример агрегации между классами

---
+++ oop/aggregation1.py
@@ -0,0 +1,48 @@
+from datetime import date, timedelta as td
+from itertools import chain
+from typing import Literal
+
+
+class Product:
+    def __init__(self, name: str, long: td, produced: date = None):
+        self.name = name
+        if produced is None:
+            produced = date.today()
+        self.produced = produced
+        self.expired: date = produced + long
+    
+    def __repr__(self):
+        return f'<{self.name!r}: {self.produced:%d.%m.%y}–{self.expired:%d.%m.%y}>'
+
+
+class Fridge:
+    def __init__(self):
+        self._cold: list[Product] = []
+        self.__freezer: list[Product] = []
+    
+    def add_product(self, *products: Product, box: Literal['c', 'f'] = 'c') -> None:
+        if box == 'c':
+            box = self._cold
+        elif box == 'f':
+            box = self.__freezer
+        else:
+            raise ValueError
+        box.extend(products)
+    
+    def __repr__(self):
+        return '\n'.join(repr(pr) for pr in chain(self._cold, self.__freezer))
+
+
+fr1 = Fridge()
+fr1.add_product(
+    Product('молоко', td(days=7)),
+    Product('сметана', td(days=14)),
+    Product('сливки', produced=date(2023, 8, 15), long=td(days=30)),
+    box='c'
+)
+fr1.add_product(
+    Product('говядина замороженная', produced=date(2023, 8, 2), long=td(days=90)),
+    Product('минтай замороженный', produced=date(2023, 7, 20), long=td(days=70)),
+    box='f'
+)
+
